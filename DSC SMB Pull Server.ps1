0204f6e1d92a7dc786ac7b8a2a3d1fb1b902c177
Mariusz Serwin <mariooserwin@gmail.com>
2015-04-03 18:16:09 +0100

    New ZIP Function
    .net Class to zip DSC Resource is having  issue , COM has to be used .

--- DSC SMB Pull Server.ps1
+++ DSC SMB Pull Server.ps1
@@ -148,7 +148,7 @@ Import-DscResource -ModuleName csmbshare,hostsfile,RecylceBin,xDisk,cFileShare,c
 
           Name = "DSC-LCM-CONFIG"
           Path = "C:\DSC-LCMCONFIG"
-          Ensure = 'present'
+          Ensure = 'absent'
           ReadAccess = 'everyone'
 
 
@@ -162,11 +162,104 @@ Import-DscResource -ModuleName csmbshare,hostsfile,RecylceBin,xDisk,cFileShare,c
 
           Name = "DSC-CONFIG"
           Path = "C:\DSC-CONFIG"
-          Ensure = 'present'
+          Ensure = 'absent'
           ReadAccess = 'everyone'
 
 
              }
+
+## 6. Assigning Quotas
+
+
+        cFolderQuota London {
+
+        Path = 'K:\London'
+        Ensure = 'present'
+        Template = 'London'
+        Subfolders = $true
+        
+
+                           }
+
+                              
+        cFolderQuota 'New York' {
+
+        Path = 'K:\New York'
+        Ensure = 'present'
+        Template = 'New York'
+        Subfolders = $true
+        DependsOn = '[cQuotaTemplate]NewYork'
+
+                             }
+
+        cFolderQuota 'Single Folder' {
+
+        Path = 'K:\Single Folder' 
+        Ensure = 'present'
+        Template = 'Generic'
+        Subfolders = $false
+        DependsOn = '[cQuotaTemplate]Generic'
+
+                             }
+
+                             
+        cFolderQuota 'Drive' {
+
+        Path = 'w:\'
+        Ensure = 'present'
+        Template = 'Generic'
+        Subfolders = $false
+        DependsOn = '[cQuotaTemplate]Generic'
+
+                             }
+
+## Creating Quotas Templates
+
+        cQuotaTemplate NewYork {
+
+        Name = 'New York'
+        Size = 11mb
+        Description = 'London Template'
+        MailTo = 'Supermario@supermario.com;Administrator@supermario.com;Supermario@supermario.com'
+        Body = 'You have Reached your Limit of Space Please Please '
+        Subject = 'We Love Arsenal :)'
+        Percentage = 12
+        Ensure = 'present'
+        SoftLimit =$false
+        
+
+        }
+        cQuotaTemplate london {
+
+        Name = 'London'
+        Size = 350mb
+        Description = 'NewYork Template'
+        MailTo = 'Supermario@supermario.com'
+        Body = 'WE Love Powershell Dont exeedec your SPACE !!!'
+        Subject = 'We Lov Powershell DSC'
+        Percentage = 11
+        Ensure = 'present'
+        SoftLimit =$false
+        
+
+        }
+
+
+
+         cQuotaTemplate Generic {
+
+        Name = 'Generic'
+        Size = 10GB
+        Description = 'User Template'
+        MailTo = 'Owner'
+        Percentage = 78
+        Ensure = 'present'
+        SoftLimit =$true
+        
+
+        }
+
+
 
                        }
                        
@@ -203,11 +296,13 @@ Import-DscResource -ModuleName csmbshare,hostsfile,RecylceBin,xDisk,cFileShare,c
 
 
  InterfaceAlias = "Internal"
- Address = "192.168.233.10","8.8.8.1"
+ Address = "192.168.233.10","8.8.8.120"
  AddressFamily = "Ipv4"
 
 
  }
+
+
 
 
 
@@ -216,17 +311,124 @@ Import-DscResource -ModuleName csmbshare,hostsfile,RecylceBin,xDisk,cFileShare,c
 }
 
 ## Zip Function
-function Zip-Directory {
-    Param(
-      [Parameter(Mandatory=$True)][string]$DestinationFileName,
-      [Parameter(Mandatory=$True)][string]$SourceDirectory,
-      [ValidateSet("Optimal", "Fastest", "NoCompression")]
-      [Parameter(Mandatory=$False)][string]$CompressionLevel = "Optimal",
-      [Parameter(Mandatory=$False)][switch]$IncludeParentDir
-    )
-    Add-Type -AssemblyName System.IO.Compression.FileSystem
-    $CompressionLevel    = [System.IO.Compression.CompressionLevel]::$CompressionLevel  
-    [System.IO.Compression.ZipFile]::CreateFromDirectory($SourceDirectory, $DestinationFileName, $CompressionLevel, $IncludeParentDir)
+Function New-ZipArchive {
+
+<#
+.Synopsis
+Create a zip archive from a folder.
+.Description
+This command will create a zip file from the specified path. The path will be a top level folder in the archive.
+.Parameter Path
+The top level folder to be archived. This parameter has aliases of PSPath and Source.
+.Parameter OutputPath
+The filename for the zip file to be created. If it already exists, the command will not run, unless you use -Force. This parameter has aliases of Zip and Target.
+.Parameter Force
+Delete the existing zip file and create a new one.
+.Example
+PS C:\> New-ZipArchive -path c:\work -outputpath e:\workback.zip 
+
+Create a new zip file called WorkBack.zip. The top level folder in the archive will be Work.
+.Example
+PS C:\> $dscres = Get-DSCResource | Select -expandproperty Module -unique | where {$_.path -notmatch "windows\\system32"}
+PS C:\> $dscres | foreach {
+ $out = "{0}_{1}.zip" -f $_.Name,$_.Version
+ $zip = Join-Path -path "E:\DSC\ZipResource" -ChildPath $out
+ New-ZipArchive -path $_.ModuleBase -OutputPath $zip -Passthru -force
+ }
+
+ The first command gets a unique list of modules for all DSC resources filtering out anything under System32. The second command creates a zip file for each module using the naming format modulename_version.zip.
+
+.Notes
+Version      : 1.0
+Last Updated : February 2, 2015
+
+Learn more about PowerShell:
+http://jdhitsolutions.com/blog/essential-powershell-resources/
+
+
+  ****************************************************************
+  * DO NOT USE IN A PRODUCTION ENVIRONMENT UNTIL YOU HAVE TESTED *
+  * THOROUGHLY IN A LAB ENVIRONMENT. USE AT YOUR OWN RISK.  IF   *
+  * YOU DO NOT UNDERSTAND WHAT THIS SCRIPT DOES OR HOW IT WORKS, *
+  * DO NOT USE IT OUTSIDE OF A SECURE, TEST SETTING.             *
+  ****************************************************************
+
+#>
+
+[cmdletbinding(SupportsShouldProcess)]
+param(
+[Parameter(Position=0,Mandatory,
+HelpMessage="Enter the folder path to be archived.")]
+[ValidateScript({Test-Path $_})]
+[Alias("PSPath","Source")]
+[String]$Path,
+[Parameter(Position=1,Mandatory,
+HelpMessage="Enter the path and filename for the zip file")]
+[Alias("zip","Target")]
+[ValidateNotNullorEmpty()]
+[String]$OutputPath,
+[Switch]$Force,
+[switch]$Passthru
+)
+
+Write-Verbose "Starting $($MyInvocation.Mycommand)"  
+Write-Verbose "Using bound parameters:"
+Write-verbose  ($MyInvocation.BoundParameters| Out-String).Trim()
+
+if ($Force -AND (Test-Path -path $OutputPath)) {
+    Write-Verbose "Testing for existing file and deleting it"
+    Remove-Item -Path $OutputPath
+}
+     
+if(-NOT (Test-Path $OutputPath)) {
+    Write-Verbose "Creating $OutputPath" 
+    Try {
+        #create an empty zip file
+        Set-Content -path $OutputPath -value ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18)) -ErrorAction Stop
+        
+        #get the zip file object
+        $zipfile = $OutputPath | Get-Item -ErrorAction Stop
+
+        #make sure it is not set to ReadOnly
+        write-verbose "Setting isReadOnly to False"
+        $zipfile.IsReadOnly = $false  
+    }
+    Catch {
+        Write-Warning "Failed to create $outputpath"
+        write-Warning $_.exception.message
+        #bail out
+        Return
+    }
+} #if not test zip file path
+else {
+    Write-Warning "The file $OutputPath already exists. Please delete or use -Force and try again."
+    
+    #bail out
+    Return
+}
+
+if ($PSCmdlet.ShouldProcess($Path)) {
+    Write-Verbose "Creating Shell.Application"
+    $shellApp = New-Object -com shell.application
+
+    Write-Verbose "Using namespace $($zipfile.fullname)" 
+    $zipPackage = $shellApp.NameSpace($zipfile.fullname)
+
+    write-verbose ($zipfile | Out-String)
+
+    $target = Get-Item -Path $Path
+
+    $zipPackage.CopyHere($target.FullName) 
+
+    If ($passthru) {
+        #Pause enough to give the zip file a chance to update
+        Start-Sleep -Milliseconds 200
+        Get-Item -Path $Outputpath
+    }
+} #should process
+
+Write-Verbose "Ending $($MyInvocation.Mycommand)"
+
 }
 
 
@@ -239,8 +441,8 @@ select -ExpandProperty module -Unique | foreach `
 $out = "{0}_{1}.zip" -f $_.Name , $_.version
 
 $zip = Join-Path  "W:\" -ChildPath $out
+New-ZipArchive -path $_.ModuleBase -OutputPath $zip
 
-Zip-Directory -DestinationFileName $zip -SourceDirectory $_.modulebase 
 
 Start-Sleep -Seconds 2 
 
@@ -257,14 +459,8 @@ New-DSCCheckSum -ConfigurationPath $zip -ErrorAction SilentlyContinue
  
 
 
-
-
-
 SmbPullServer  -ConfigurationData $myconfig -OutputPath "C:\DSC-CONFIG"
-
-
-
-
+Publish-MrMOFToSMB "C:\DSC-CONFIG"
 
 
 Start-DscConfiguration -Path "C:\DSC-CONFIG"   -Wait -Verbose -Force
@@ -369,3 +565,130 @@ $myconfig = `
 
 
 }
+
+function Publish-MrMOFToSMB {
+
+<#
+.SYNOPSIS
+    Publishes a DSC MOF configuration file to the pull server that's configured on a target node(s).
+ 
+.DESCRIPTION
+    Publish-MrMOFToSMB is an advanced PowerShell function that publishes one or more MOF configuration files
+    to the an SMB DSC server by determining the ConfigurationID (GUID) that's configured on the target node along
+    with the UNC path of the SMB pull server and creates the necessary checksum along with copying the MOF and
+    checksum to the pull server.
+ 
+.PARAMETER ConfigurationPath
+    The folder path on the local computer that contains the mof configuration files.
+
+.PARAMETER ComputerName
+    The computer name of the target node that the DSC configuration is created for.
+ 
+.EXAMPLE
+     Publish-MrMOFToSMB -ConfigurationPath 'C:\MyMofFiles'
+
+.EXAMPLE
+     Publish-MrMOFToSMB -ConfigurationPath 'C:\MyMofFiles' -ComputerName 'Server01', 'Server02'
+
+.EXAMPLE
+     'Server01', 'Server02' | Publish-MrMOFToSMB -ConfigurationPath 'C:\MyMofFiles'
+
+.EXAMPLE
+     MyDscConfiguration -Param1 Value1 -Parm2 Value2 | Publish-MrMOFToSMB
+ 
+.INPUTS
+    String
+ 
+.OUTPUTS
+    None
+ 
+.NOTES
+    Author:  Mike F Robbins
+    Website: http://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [ValidateScript({Test-Path -Path $_ -PathType Container})]
+        [Alias('Directory')]
+        [string]$ConfigurationPath,
+
+        [Parameter(ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [Alias('BaseName')]
+        [string[]]$ComputerName
+    )
+
+    BEGIN {
+        if (-not($PSBoundParameters['ComputerName'])) {
+            $ComputerName = (Get-ChildItem -Path $ConfigurationPath\*.mof).basename
+        }
+    }
+
+    PROCESS {
+        foreach ($Computer in $ComputerName) {
+
+            try {
+                Write-Verbose -Message "Retrieving LCM information from $Computer"
+                $LCMConfig = Get-DscLocalConfigurationManager -CimSession $Computer -ErrorAction Stop
+            }
+            catch {
+                Write-Error -Message "An error has occurred. Error details: $_.Exception.Message"
+                continue
+            }        
+            
+            $servermof = "$ConfigurationPath\$Computer.mof"
+
+            if (-not(Get-ChildItem -Path $servermof -ErrorAction SilentlyContinue)) {
+                Write-Error -Message "Unable to find MOF file for $Computer in location: $ConfigurationPath"
+            } 
+            elseif ($LCMConfig.RefreshMode -ne 'Pull') {
+                Write-Error -Message "The LCM on $Computer is not configured for DSC pull mode."
+            }
+            elseif ($LCMConfig.DownloadManagerName -ne 'DscFileDownloadManager' -and $LCMConfig.ConfigurationDownloadManagers.ResourceId -notlike '`[ConfigurationRepositoryShare`]*') {
+                Write-Error -Message "LCM on $Computer not configured to receive configuration from DSC SMB pull server"
+            }
+            elseif (-not($LCMConfig.ConfigurationID)) {
+                Write-Error -Message "A ConfigurationID (GUID) has not been set in the LCM on $Computer"
+            }
+            else {
+                if ($LCMConfig.ConfigurationDownloadManagers.SourcePath) {
+                    $SMBPath = "$($LCMConfig.ConfigurationDownloadManagers.SourcePath)"
+                }
+                elseif ($LCMConfig.DownloadManagerCustomData.Value) {
+                    $SMBPath = "$($LCMConfig.DownloadManagerCustomData.Value)"
+                }
+
+                Write-Verbose -Message "Creating DSCChecksum for $servermof"
+                New-DSCCheckSum -ConfigurationPath $servermof -Force
+
+                if (Test-Path -Path $SMBPath) {
+
+                    $guidmof = Join-Path -Path $SMBPath -ChildPath "$($LCMConfig.ConfigurationID).mof"
+
+                    try {
+                        Write-Verbose -Message "Copying $servermof.checksum to $guidmof.checksum"
+                        Copy-Item -Path "$servermof.checksum" -Destination "$guidmof.checksum" -ErrorAction Stop
+
+                        Write-Verbose -Message "Copying $servermof to $guidmof"
+                        Copy-Item -Path $servermof -Destination $guidmof -ErrorAction Stop
+                    }
+                    catch {
+                        Write-Error -Message "An error has occurred. Error details: $_.Exception.Message"                    
+                    }
+
+                }
+                else {
+                    Write-Error -Message "Unable to connect to $SMBPath as specified in the LCM on $Computer for it's DSC pull server"
+                }
+            }
+        }
+    }
+}
+
+
+Test-DscConfiguration -CimSession windows8 -Verbose
+Test-DscConfiguration  -Verbose
